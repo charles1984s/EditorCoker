58909972912ecdea55fbc7c7681c5aa0e109192a
charles1984s <charles@ether.com.tw>
2023-10-19 16:15:12 +0800

    [update] grapes繁體中文化

--- wwwroot/js/Grapes/locale/tw.js
+++ wwwroot/js/Grapes/locale/tw.js
@@ -1,51 +1,51 @@
-﻿const traitInputAttr = { placeholder: '例子. 输入文字' };
+﻿const traitInputAttr = { placeholder: '例如. 輸入文字' };
 
 let tw = {
     assetManager: {
-        addButton: '添加图片',
+        addButton: '新增圖片',
         inputPlh: 'http://path/to/the/image.jpg',
-        modalTitle: '选择图片',
-        uploadTitle: '点击或者拖拽图片上传',
+        modalTitle: '選擇圖片',
+        uploadTitle: '點擊或拖曳圖片上傳',
     },
     domComponents: {
         names: {
             '': 'Box',
             wrapper: 'Body',
             text: '文字',
-            comment: '评论',
-            image: '图片',
-            video: '视频',
-            label: '文本',
-            link: '超链接',
-            map: '地图',
-            tfoot: '表格末尾',
-            tbody: '表格主体',
-            thead: '表头',
+            comment: '評論',
+            image: '圖片',
+            video: '影片',
+            label: '文字',
+            link: '超連結',
+            map: '地圖',
+            tfoot: '表尾',
+            tbody: '表身',
+            thead: '表頭',
             table: '表格',
             row: '行',
-            cell: '单元格',
+            cell: '儲存格',
         },
     },
     deviceManager: {
-        device: '设备',
+        device: '設備',
         devices: {
-            desktop: '桌面',
+            desktop: '電腦',
             tablet: '平板',
-            mobileLandscape: 'Mobile Landscape',
-            mobilePortrait: 'Mobile Portrait',
+            mobileLandscape: '手機橫放',
+            mobilePortrait: '手機垂直',
         },
     },
     panels: {
         buttons: {
             titles: {
-                preview: '预览',
-                fullscreen: '全屏',
-                'sw-visibility': '查看组件',
-                'export-template': '查看代码',
-                'open-sm': '打开样式管理器',
-                'open-tm': '设置',
-                'open-layers': '打开布局管理器',
-                'open-blocks': '打开块',
+                preview: '預覽',
+                fullscreen: '全螢幕',
+                'sw-visibility': '查看組件',
+                'export-template': '查看原始碼',
+                'open-sm': '打開樣式管理氣',
+                'open-tm': '設定',
+                'open-layers': '打開布局管理氣',
+                'open-blocks': '打開區塊',
             },
         },
     },
@@ -60,16 +60,16 @@ let tw = {
         },
     },
     styleManager: {
-        empty: '设置样式前选择请一个元素',
-        layer: '层级',
-        fileButton: '图片',
+        empty: '設定樣式前，請選擇一個元素',
+        layer: '階層',
+        fileButton: '圖片',
         sectors: {
-            general: '常规',
-            layout: '布局',
-            typography: '版式',
-            decorations: '装饰',
-            extra: '扩展',
-            flex: '盒子模型',
+            general: '常規',
+            layout: '排版',
+            typography: '版祥',
+            decorations: '設計',
+            extra: '擴展',
+            flex: '模型',
             dimension: '尺寸',
         },
         // The core library generates the name by their `property` name
@@ -78,8 +78,8 @@ let tw = {
         },
     },
     traitManager: {
-        empty: '用设置项前选择一个组件',
-        label: '组件设置',
+        empty: '設定前請選擇一個元素',
+        label: '組件設定',
         traits: {
             // In a simple trait, like text input, these are used on input attributes
             attributes: {
@@ -91,8 +91,8 @@ let tw = {
             // In a trait like select, these are used to translate option names
             options: {
                 target: {
-                    false: '本窗口',
-                    _blank: '新窗口',
+                    false: '直接連結',
+                    _blank: '另開新視窗',
                 },
             },
         },
