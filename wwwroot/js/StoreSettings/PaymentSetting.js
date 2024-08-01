7e52f5edd26e7f9945768002202bbd38e43d9915
charles1984s <charles@ether.com.tw>
2024-08-01 10:43:53 +0800

    [update] SEO、付費方式設定修改

---
+++ wwwroot/js/StoreSettings/PaymentSetting.js
@@ -0,0 +1,12 @@
+﻿function PageReady() {
+    const formId = "StoreSet";
+    console.log($(`#${formId}`));
+    co.Form.set(formId, () => {
+        console.log(co.Object.objectToArray(co.Form.getJson(formId, true)));
+        /*co.StoreSet.SaveValues(co.Object.objectToArray(co.Form.getJson(formId, true))).done(function (result) {
+            if (result.success) co.sweet.success("儲存成功");
+            else co.sweet.error("儲存失敗", result.message);
+        });*/
+        return false;
+    });
+}
