40dd6927a5d0d62ea02d0faac0e407557489fec8
charles1984s <charles@ether.com.tw>
2024-11-11 02:16:55 +0800

    [update] 增加後台CSS 上稿

---
+++ wwwroot/js/ContentManagement/SettingCss.js
@@ -0,0 +1,15 @@
+﻿function PageReady() {
+    const formId = "SettingCss";
+    co.Form.set(formId, function () {
+        const data = co.Form.getJson(formId);
+        co.sweet.confirm("是否確認儲存?", "即將將您設定的css發佈在前台所有網站上!", "確認", "取消", function () {
+            co.WebSite.SettingCss(data.Css).done(function (result) {
+                if (result.success) co.sweet.success("儲存成功!");
+                else co.sweet.error("失敗", result.error)
+            });
+        });
+    });
+    co.WebSite.LoadFrameCss().done(function (result) {
+        co.Form.insertData({ Css: result.message });
+    });
+}
