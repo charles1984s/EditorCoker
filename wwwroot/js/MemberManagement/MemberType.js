e383b6d85a2aa542091ccef13198b3316a79ab61
charles1984s <charles@ether.com.tw>
2024-11-12 10:24:40 +0800

    [update] ValidateAntiForgeryToken 驗證異常，暫時關閉

---
+++ wwwroot/js/MemberManagement/MemberType.js
@@ -0,0 +1,22 @@
+﻿let dxData;
+function PageReady() {
+    
+}
+
+function contentReady(e) {
+    dxData = e.component;
+}
+
+function editButtonClicked(e) {
+    var dataGrid = e.component;
+    dataGrid.option('editing.editRowKey', e.row.key);
+}
+
+function deleteButtonClicked(e) {
+    co.sweet.confirm("刪除角色", "確定刪除？角色刪除後不可復原", "確　定", "取　消", function () {
+        /*co.Tag.TagGroupDelete(e.row.key).done(function () {
+            GroupList_dxData.refresh();
+            TagList_dxData.refresh();
+        })*/
+    })
+}
